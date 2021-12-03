fcee7144e16a9963e20a7c14fb72fbe0cef1435f
edgariph98 <edgar_ivanhdz15@hotmail.com>
2021-12-03 09:26:13 -0600

    Starting 3D Graphics React

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react';
+import './App.scss';
+import { Canvas, useFrame } from "react-three-fiber";
+// import { Box } from "drei";
+import * as THREE from "three";
+import { Vector3, Mesh } from 'three';
+const Box  = (props: { position: Vector3 }) => {
+  const refMesh = useRef<Mesh>(null);
+
+  useFrame(() => {
+    if (refMesh.current) {
+      // rotates the object
+      return refMesh.current.rotation.y = refMesh.current.rotation.x += 0.01;
+    }
+  });
+  return (
+    <mesh ref={refMesh}  scale={[1,1,1]}position={props.position}>
+      <boxBufferGeometry attach="geometry" args={[1,1,1]} />
+      <meshStandardMaterial attach="material" color="blue" />
+    </mesh>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Canvas camera={{position: [-5,2,10], fov: 60}}>
+        <ambientLight intensity={0.3}  />
+        <Box position={new THREE.Vector3(0, 0, 0)} ></Box>
+        <Box position={new THREE.Vector3(-4, 0, 0)} ></Box>
+        <Box position={new THREE.Vector3(4, 0, 0)}></Box>
+      </Canvas>
+    </>
   );
 }
 
